6183632ec8a2d850e73ea3d4d55b79af8ee598a8
Jason Cheatham <jason@jasoncheatham.com>
2022-01-07 13:54:49 -0500

    Add more SDK types

--- mistySdk.d.ts
+++ mistySdk.d.ts
@@ -1,6 +1,62 @@
 export {};
 
 declare global {
+  type EventType =
+    | "ActuatorPosition"
+    | "ArTagDetection"
+    | "AudioPlayComplete"
+    | "BatteryCharge"
+    | "BumpSensor"
+    | "ChargerPoseMessage"
+    | "CriticalStatusMessage"
+    | "DriveEncoders"
+    | "FaceRecognition"
+    | "FaceTraining"
+    | "HaltCommand"
+    | "HazardNotification"
+    | "IMU"
+    | "KeyPhraseRecognized"
+    | "LocomotionCommand"
+    | "ObjectDetection"
+    | "PRUMessage"
+    | "SelfState"
+    | "SerialMessage"
+    | "SkillData"
+    | "SkillSystemStateChange"
+    | "SlamStatus"
+    | "SourceTrackDataMessage"
+    | "SourceFocusConfigMessage"
+    | "TextToSpeechComplete"
+    | "TimeOfFlight"
+    | "TouchSensor"
+    | "VoiceRecord"
+    | "WorldState";
+
+  type CallbackRule =
+    | "synchronous"
+    | "override"
+    | "abort";
+
+  type Inequality =
+    | "=>"
+    | "=="
+    | "!=="
+    | ">"
+    | "<"
+    | ">="
+    | "<="
+    | "exists"
+    | "empty"
+    | "delta";
+
+  type ValueType =
+    | "double"
+    | "float"
+    | "integer"
+    | "string"
+    | "datetime"
+    | "boolean";
+
   interface Misty {
     /**
      * Changes the color of the LED light behind the logo on Misty's torso.
@@ -9,6 +65,71 @@ declare global {
       red: number,
       green: number,
       blue: number,
+      prePauseMs?: number,
+      postPauseMs?: number,
+    ): void;
+
+    /**
+     * Drives Misty forward or backward at a specific speed until cancelled.
+     */
+    Drive(
+      linearVelocity: number,
+      angularVelocity: number,
+      prePauseMs?: number,
+      postPauseMs?: number,
+    ): void;
+
+    /**
+     * Drives Misty forward or backward at a set speed, with a given rotation,
+     * for a specified amount of time.
+     */
+    DriveTime(
+      /** -100 (backward) to 100 (forward) */
+      linearVelocity: number,
+      /** -100 (rotate clockwise) to 100 (rotate counterclockwise) */
+      angularVelocity: number,
+      timeMs: number,
+      degree?: number,
+      prePauseMs?: number,
+      postPauseMs?: number,
+    ): void;
+
+    /**
+     * Creates a listener that receives live data from one of Misty's event
+     * types.
+     */
+    RegisterEvent(
+      /**
+       * The name of the callback function is implicitly set to the
+       * `_<eventName>`
+       */
+      eventName: string,
+      messageType: EventType,
+      debounce: number,
+      property?: string,
+      inequality?: Inequality,
+      valueAsString?: string,
+      valueType?: ValueType,
+      callbackRule?: CallbackRule,
+      skillToCall?: string,
+      prePauseMs?: number,
+      postPauseMs?: number,
+    ): void;
+
+    /**
+     * Registers for an event and applies a filter to event messages.
+     */
+    RegisterSimpleEvent(
+      /**
+       * The name of the callback function is implicitly set to the
+       * `_<eventName>`
+       */
+      eventName: string,
+      messageType: EventType,
+      debounce: number,
+      keepAlive?: boolean,
+      callbackRule?: CallbackRule,
+      skillToCall?: string,
       prePauseMs?: number,
       postPauseMs?: number,
     ): void;
@@ -36,32 +157,10 @@ declare global {
       postPauseMs?: number,
     ): void;
 
+    /**
+     * Turns the LED flashlight on Misty's head on or off.
+     */
     SetFlashlight(on: boolean, prePauseMs?: number, postPauseMs?: number): void;
-
-    /**
-     * Drives Misty forward or backward at a specific speed until cancelled.
-     */
-    Drive(
-      linearVelocity: number,
-      angularVelocity: number,
-      prePauseMs?: number,
-      postPauseMs?: number,
-    ): void;
-
-    /**
-     * Drives Misty forward or backward at a set speed, with a given rotation,
-     * for a specified amount of time.
-     */
-    DriveTime(
-      /** -100 (backward) to 100 (forward) */
-      linearVelocity: number,
-      /** -100 (rotate clockwise) to 100 (rotate counterclockwise) */
-      angularVelocity: number,
-      timeMs: number,
-      degree?: number,
-      prePauseMs?: number,
-      postPauseMs?: number,
-    ): void;
   }
 
   const misty: Misty;
