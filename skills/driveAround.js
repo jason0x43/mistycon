9bbf90ad18bc5237322b18e1abaa71658f404b0f
Jason Cheatham <jason@jasoncheatham.com>
2022-01-07 22:51:19 -0500

    Rename look-around, add driveAround

---
+++ skills/driveAround.js
@@ -0,0 +1,30 @@
+function _hazard(data) {
+  const triggers = [];
+  for (const sensorType of data.AdditionalResults) {
+    for (const sensor of sensorType) {
+      if (sensor.InHazard) {
+        triggers.push(sensor.SensorName);
+      }
+    }
+  }
+
+  if (triggers.length > 0) {
+    misty.Debug(`Saw hazards: ${JSON.stringify(triggers)}`);
+    misty.Stop();
+    misty.ChangeLED(255, 0, 0);
+    misty.Pause(3000);
+    misty.CancelSkill("0ddd028b-6594-41b6-8769-0eaa88402293");
+  }
+}
+
+function _halt() {
+  misty.Debug("Robot halted");
+}
+
+misty.AddReturnProperty("hazard", "BumpSensorsHazardState");
+misty.AddReturnProperty("hazard", "TimeOfFlightSensorsHazardState");
+misty.RegisterEvent("hazard", "HazardNotification", 0, true);
+misty.RegisterEvent("halt", "HaltCommand", 0, true);
+
+misty.ChangeLED(0, 255, 0);
+misty.Drive(10, 0);
---
+++ skills/lookAround.js
@@ -0,0 +1,15 @@
+function _bumped(data) {
+  const sensor = data.AdditionalResults[0];
+  const isPressed = data.AdditionalResults[1];
+  isPressed
+    ? misty.Debug(`${sensor} is pressed`)
+    : misty.Debug(`${sensor} is released`);
+}
+
+misty.AddReturnProperty("bumped", "sensorName");
+misty.AddReturnProperty("bumped", "IsContacted");
+misty.RegisterEvent("bumped", "BumpSensor", 100, true);
+
+setTimeout(function () {
+  misty.Debug("loaded");
+}, 2000);
